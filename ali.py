c8896134b1aa6d9c96ed074e09f0c62e030738c8
gyovanesouzza <gyovanesouzza@outlook.com.br>
2021-07-23 22:55:30 -0300

    Correção bug

--- ali.py
+++ ali.py
@@ -1,35 +1,38 @@
 import pyautogui
 from pyautogui import locateOnScreen as LoS
 from time import sleep
+from datetime import datetime
 
-try:
-    sleep(10)
-    while True:
 
-        CUPONS = ['2021ali20', 'AECOMPRA20', 'alipay20', 'alitec20', 'awintop20', 'besmart20', 'chinacupon20',
-              'cupomvalido20', 'importchina20', 'sav20', 'superdicas20', "tiagolima20", "SANTOSTEC20", 'joaoali20']
+sleep(3)
+while True:
 
-        TOTAL = LoS('total.png', confidence=0.8)
-        if TOTAL:
-            for cupom in CUPONS:
-                print(cupom)
-                sleep(1)
+    CUPONS = ['2021ali20', 'AECOMPRA20','alipay20', 'alitec20', 'awintop20', 'chinacupon20',
+              'cupomvalido20', 'importchina20', 'superdicas20', "tiagolima20", "SANTOSTEC20", 'joaoali20']
 
-                x, y = pyautogui.locateCenterOnScreen('text.png', confidence=0.8)
-                pyautogui.click(x, y)
-                pyautogui.write(cupom)
-                sleep(2)
-                z, t = pyautogui.locateCenterOnScreen(
-                'utilizar.png', confidence=0.8)
+    TOTAL = LoS('total.png', confidence=0.8)
+    if TOTAL:
+        for cupom in CUPONS:
+            print(cupom)
+            texto = LoS('text.png', confidence=0.8)
+            if texto is None :
+                z, t = pyautogui.locateCenterOnScreen('fazer.png', confidence=0.8)
+                print(datetime.now() + " Foi aplicado o Cupom: " + cupom)   
                 pyautogui.click(z, t)
-                sleep(2)
+                break
 
-                if LoS('utilizar.png', confidence=0.8) is None:
-                    break
+           
+            x, y = pyautogui.locateCenterOnScreen('text.png', confidence=0.8)
+            pyautogui.click(x, y)
+            sleep(1)
+            pyautogui.write(cupom)
+            sleep(1.5)
 
-        else:
-            break
-    z, t = pyautogui.locateCenterOnScreen('fazer.png', confidence=0.8)
-    pyautogui.click(z, t)
-except :
-    print("Tire screeshot dos botões novamente")
+            z, t = pyautogui.locateCenterOnScreen(
+            'utilizar.png', confidence=0.8)
+            pyautogui.click(z, t)
+
+            sleep(2)
+           
+    else:
+        break 
